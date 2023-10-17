74f173b8d7ab2798f6e772a4f67ec7fb516ec537
Binyamin Tamirat <biny01amin@gmail.com>
2023-10-17 12:16:37 +0300

    inital commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+// import "./LayoutCss.css";
+// import Layout from "./layout";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    {/* <Layout /> */}
+  </React.StrictMode>
+);
